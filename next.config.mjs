75a00f795875d17ae3391181c2e2099bc59b1b61
AHpx <ahpx@yandex.com>
2025-04-23 14:44:37 +0800

    chore: update nextjs config to setup mastra

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
     // set basepath based on environment
     basePath: process.env.NEXT_PUBLIC_BASE_PATH ?? "",
     output: "standalone",
+    serverExternalPackages: ["@mastra/*"],
 };
 
 export default nextConfig;
